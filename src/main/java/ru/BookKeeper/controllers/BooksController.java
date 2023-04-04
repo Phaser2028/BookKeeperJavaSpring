962f6f0d4684cfcd22abd197610d943af752fbb5
Phaser2028 <dima.2000600@gmail.com>
2023-04-04 19:13:02 +0300

    controller

---
+++ src/main/java/ru/BookKeeper/controllers/BooksController.java
@@ -0,0 +1,139 @@
+package ru.BookKeeper.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.BookKeeper.dao.BookDAO;
+import ru.BookKeeper.dao.PersonDAO;
+import ru.BookKeeper.models.Book;
+import ru.BookKeeper.models.Person;
+
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/books")
+public class BooksController {
+
+    private final BookDAO bookDAO;
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public BooksController(BookDAO bookDAO, PersonDAO personDAO) {
+        this.bookDAO = bookDAO;
+        this.personDAO = personDAO;
+    }
+
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("books", bookDAO.index());
+        return "books/index";
+    }
+
+
+
+
+    ////важное событие тут
+    ////
+    ////
+    ////
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id,@ModelAttribute("person") Person person,Model model) {
+        model.addAttribute("book", bookDAO.show(id));
+
+        Optional<Person> bookOwner = bookDAO.getBookOwner(id);
+
+        if(bookOwner.isPresent()){
+            model.addAttribute("owner",bookOwner.get());
+        }
+        else {
+            model.addAttribute("people",personDAO.index());
+        }
+        return "books/show";
+    }
+
+
+
+
+
+
+    @PostMapping("/{id}/assign")
+    public String assign(@PathVariable("id") int id, @ModelAttribute("person") Person person){
+
+        bookDAO.assignBook(id,person);//присваиваем книгу с айди = id пользователю = person
+
+        return "redirect:/books/"+id;
+    }
+
+
+
+
+
+    @PostMapping("/{id}/back")
+    public String getBack(@PathVariable("id") int id,@ModelAttribute("person") Person person,Model model){
+
+
+        bookDAO.getBackBook(id);//забрать книгу обратно
+
+        return "redirect:/books";
+    }
+
+
+
+
+
+
+
+
+    @GetMapping("/new")
+    public String newBook(@ModelAttribute("book") Book book) {
+        return "books/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("book") @Valid Book book,
+                         BindingResult bindingResult) {
+
+
+        if (bindingResult.hasErrors()) {
+            return "books/new";
+        }
+        bookDAO.save(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("book", bookDAO.show(id));
+        return "books/edit";
+    }
+
+    @PostMapping("/{id}")
+    public String update(@ModelAttribute("book") @Valid Book book, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+
+
+
+        if (bindingResult.hasErrors())
+            return "books/edit";
+
+        bookDAO.update(id, book);
+        return "redirect:/books";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        bookDAO.delete(id);
+        return "redirect:/books";
+    }
+
+
+}
---
+++ src/main/java/ru/BookKeeper/controllers/PeopleController.java
@@ -0,0 +1,90 @@
+package ru.BookKeeper.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.BookKeeper.dao.BookDAO;
+import ru.BookKeeper.dao.PersonDAO;
+import ru.BookKeeper.models.Book;
+import ru.BookKeeper.models.Person;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PersonDAO personDAO;
+    private final BookDAO bookDAO;
+
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO, BookDAO bookDAO) {
+        this.personDAO = personDAO;
+        this.bookDAO = bookDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id,Model model) {
+        model.addAttribute("person", personDAO.show(id));
+        model.addAttribute("books",  personDAO.getBooksByPersonId(id));//// тут
+        return "people/show";
+    }
+
+
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+//        personValidator.validate(person,bindingResult);
+
+        if (bindingResult.hasErrors())
+            return "people/new";
+
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+
+    @PostMapping("/{id}")
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+
+
+
+        if (bindingResult.hasErrors())
+            return "people/edit";
+
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDAO.delete(id);
+        return "redirect:/people";
+    }
+
+
+
+}
